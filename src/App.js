5aa59c5a48405392ddaef49121926d52eefa59f1
NIHALKURUNIYAN <nihalkuruniyan555@gmail.com>
2024-01-19 01:54:28 -0800

    change

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import akil from './assets/akil.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={akil} className="App-logo" alt="logo" />
+        <p>
+          Akhil
+        </p>
+        <a href='https://www.instagram.com/akkineniakhil/?hl=en'>Follow me on instagram</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
